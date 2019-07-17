loop in svars_cv is working,but with wrong results

svars_cv.py
@@ -12,13 +12,13 @@ from __future__ import print_function, division
 import numpy as np
 import numpy.linalg as npl
 from numpy.linalg import slogdet
-from scipy.linalg import solve
+# from scipy.linalg import solve
 import pandas as pd 
 from functools import partial
 import itertools
 from scipy.stats import chi2
 from scipy.optimize import minimize 
-# from numpy.linalg import solve
+from numpy.linalg import solve
 
 from statsmodels.tools.numdiff import (approx_hess, approx_fprime)
 from statsmodels.tools.decorators import cache_readonly
@@ -61,25 +61,27 @@ class SVAR_CV(tsbase.TimeSeriesModel):
         restrictions = 0
     
         if restriction_matrix is not None:
-            result_unrestricted = self._identify_volatility(endog ,SB=SB, u=self.u, k=self.k, y=None, restriction_matrix=None, sigma_hat1=sigma_hat1,\
-        sigma_hat2=sigma_hat2, p=self.p, TB=TB, crit=crit, y_out=None, type=self.type)
-            result = self._identify_volatility(endog ,SB=SB, u=self.u, k=self.k, y=None, restriction_matrix=restriction_matrix, restrictions=restrictions, sigma_hat1=sigma_hat1,\
-        sigma_hat2=sigma_hat2, p=self.p, TB=TB, crit=crit, y_out=None, type=self.type)
-            l_ratio_test_statistics = 2 * (result_unrestricted.lik - result.lik)
+            result_unrestricted = self._identify_volatility(endog ,SB=SB, u=self.u, k=self.k, y=self.y, restriction_matrix=None, restrictions=restrictions, sigma_hat1=sigma_hat1,\
+        sigma_hat2=sigma_hat2, p=self.p, TB=TB, SB_character=self.SB_character, crit=crit, y_out=self.y_out, type_=self.type_)
+            result = self._identify_volatility(endog ,SB=SB, u=self.u, k=self.k, y=self.y, restriction_matrix=restriction_matrix, restrictions=restrictions, sigma_hat1=sigma_hat1,\
+        sigma_hat2=sigma_hat2, p=self.p, TB=TB, SB_character=self.SB_character, crit=crit, y_out=self.y_out, type_=self.type_)
+            l_ratio_test_statistics = 2 * (result_unrestricted['lik'] - result['lik'])
             #TODO: pchisq aus R herausfinden
-            p_value = np.round(1 - pchisq(l_ratio_test_statistics, result.restrictions), 4)
+            p_value = np.round(1 - chi2.ppf(l_ratio_test_statistics, result['restrictions']), 4)
 
             #TODO: Dataframe-Magie umsetzen ;)
             l_ratio_test = pd.DataFrame()
         else:
             restriction_matrix = None
             result = self._identify_volatility(endog ,SB=SB, u=self.u, k=self.k, y=self.y , restriction_matrix=restriction_matrix, restrictions=restrictions, sigma_hat1=sigma_hat1,\
-                                                sigma_hat2=sigma_hat2, p=self.p, TB=TB, crit=crit, y_out=None, type=self.type)
-    """
+                                                sigma_hat2=sigma_hat2, p=self.p, TB=TB, SB_character=self.SB_character, crit=crit, y_out=self.y_out, type_=self.type_)
+        
+        print(result)
+        """
          
         #TODO: Skript zum Testen der Klasse schreiben!!!
-    """
-
+        """
+        
     def _get_var_object(self, endog):
         """Get VAR object from Class VARResultsWrapper"""
         if(type(endog) != VARResultsWrapper):
@@ -90,8 +92,8 @@ class SVAR_CV(tsbase.TimeSeriesModel):
         self.residY = endog.resid
         self.p = endog.k_ar
         self.y = endog.endog.T
-        self.yOut = endog.endog
-        self.type = endog.trend        
+        self.y_out = endog.endog
+        self.type_ = endog.trend        
         self.coef_x = endog.coefs
 
     def _get_structural_break(self, SB, date_vector=None, start=None, end=None, \
@@ -112,7 +114,7 @@ class SVAR_CV(tsbase.TimeSeriesModel):
         return list(np.where([datetime.strptime(SB) in i for i in date_vector])[0])
     
     def _identify_volatility(self, x, SB, u, k, y, restriction_matrix, restrictions, sigma_hat1,\
-        sigma_hat2, p, TB, crit, y_out, type):
+        sigma_hat2, p, TB, SB_character, crit, y_out, type_):
         if restriction_matrix is not None:
             B = np.linalg.cholesky((1/self.Tob) * (u.T@u)).T
             na_elements = np.isnan(restriction_matrix)
@@ -125,8 +127,8 @@ class SVAR_CV(tsbase.TimeSeriesModel):
         lambda_ = np.ones(k)
         S = np.concatenate((B, lambda_), axis=None)
 
-        #TODO: Changing the method and set jac on true
-        MLE = minimize(fun=self._likelihood, x0=S, args=(self.Tob, TB, sigma_hat1, k, sigma_hat2, restriction_matrix, restrictions),method='BFGS', options={"maxiter": 150}) 
+        #minimize is working fine
+        MLE = minimize(fun=self._likelihood, x0=S, args=(self.Tob, TB, sigma_hat1, k, sigma_hat2, restriction_matrix, restrictions),method='BFGS', tol=1e-6, options={"maxiter": 150}) 
         
         if restriction_matrix is not None:
             na_elements = np.isnan(restriction_matrix)
@@ -134,7 +136,7 @@ class SVAR_CV(tsbase.TimeSeriesModel):
             B_hat[na_elements] = MLE.x[0:sum(na_elements)]
             lambda_hat = np.diag(MLE.x[sum(na_elements) : len(MLE.x)])
         else:
-            B_hat = np.array(MLE.x[0:(k*k)]).reshape((-1,k))
+            B_hat = np.array(MLE.x[0:(k*k)]).reshape((-1,k)).T
             lambda_hat = np.diag(MLE.x[(k*k):(k*k+k)])
             restrictions = 0
         
@@ -169,9 +171,9 @@ class SVAR_CV(tsbase.TimeSeriesModel):
             sig2 = solve(B_temp, np.eye(B_temp.shape[0]))
             
             GLS1_1 = np.sum(np.apply_along_axis(self._gls1, axis=0, arr=Z_t[:,:(TB-1)], sig=sig1), axis=1) #sum of each row
-            GLS1_2 = np.sum(np.apply_along_axis(self._gls1, axis=0, arr=Z_t[:,TB:], sig=sig1), axis=1) #sum of each row
+            GLS1_2 = np.sum(np.apply_along_axis(self._gls1, axis=0, arr=Z_t[:,TB:], sig=sig2), axis=1) #sum of each row
             if x.trend == None:
-                GLS1_temp = (GLS1_1 + GLS1_2).reshape((k*k*p,-1)) 
+                GLS1_temp = (GLS1_1 + GLS1_2).reshape((k*k*p,-1))
                 GLS1 = solve(GLS1_temp, np.eye(GLS1_temp.shape[0]))
                 GLS2_1 = np.zeros((k*k*p, TB-1))
                 GLS2_2 = np.zeros((k*k*p, y.shape[1]))
@@ -181,7 +183,7 @@ class SVAR_CV(tsbase.TimeSeriesModel):
                 GLS2_1 = np.zeros((k*k*p+k, TB-1))
                 GLS2_2 = np.zeros((k*k*p+k, y.shape[1]))
             elif x.trend == 'b':
-                GLS1_temp = (GLS1_1 + GLS1_2).reshape((k*k*p+k+k,-1)) 
+                GLS1_temp = (GLS1_1 + GLS1_2).reshape((k*k*p+k+k,-1))
                 GLS1 = solve(GLS1_temp, np.eye(GLS1_temp.shape[0]))
                 GLS2_1 = np.zeros((k*k*p+k+k, TB-1))
                 GLS2_2 = np.zeros((k*k*p+k+k, y.shape[1]))
@@ -194,40 +196,42 @@ class SVAR_CV(tsbase.TimeSeriesModel):
             GLS2_1 = np.sum(GLS2_1, axis=1)
             GLS2_2 = np.sum(GLS2_2, axis=1)
             GLS2 = GLS2_1 + GLS2_2
+            GLS2.reshape(-1,1)
 
+            #GLS1 is fine and GLS2 also but GLS_hat is wrong
             GLS_hat = GLS1 @ GLS2
-
             term1 = np.apply_along_axis(partial(self._resid_gls, k=k, GLS_hat=GLS_hat), axis=0, arr=Z_t)
             ugls = y.T - term1.T
 
-            resid1gls = ugls[:TB-1,]
+            resid1gls = ugls[0:TB-1,]
             resid2gls = ugls[TB-1:self.Tob,]
-            sigma_hat1gls = resid1gls.T @ resid1gls / (TB-1) 
-            sigma_hat2gls = resid2gls.T @ resid2gls / (self.Tob-TB+1)
+            sigma_hat1gls = (resid1gls.T@resid1gls) / (TB-1) 
+            sigma_hat2gls = (resid2gls.T@resid2gls) / (self.Tob-TB+1)
 
             # Determine starting values for B and Lambda
             if restriction_matrix is not None:
                 B = np.linalg.cholesky(1/self.Tob * (u.T@u)).T
                 na_elements = np.isnan(restriction_matrix)
                 B = B[na_elements]
-                restrictions = len(restriction_matrix[~np.isnan(restriction_matrix)])
+                restrictions = len(restriction_matrix[not np.isnan(restriction_matrix)])
             else:
                 B = np.linalg.cholesky(1/self.Tob * (u.T@u)).T
 
-            lambda_ = np.ones((k,1))
-            S = np.concatenate((B, lambda_), axis=None)
+            lambda_ = np.ones(k)
+            S = np.concatenate((B, lambda_), axis=None) # S is not changing in the loop
 
-            # optimize the likelihood function
-            # TODO: Übergeben von Parametern
-            MLE_gls = minimize(fun=self._likelihood, x0=S, args=(self.Tob, TB, sigma_hat1gls, k, sigma_hat2gls, restriction_matrix, restrictions),method='BFGS', jac=False, options={"maxiter": 150}) 
+            # optimize the likelihood function 
+            # TODO: Fix error in minimize (sigma_hat1gls or sigma_hat2gls must be false)
+            MLE_gls = minimize(fun=self._likelihood, x0=S, args=(self.Tob, TB, sigma_hat1gls, k, sigma_hat2gls, restriction_matrix, restrictions),method='BFGS',tol=1e-6, options={"maxiter": 350}) 
 
             if restriction_matrix is not None:
                 na_elements = np.isnan(restriction_matrix)
                 B_hatg = restriction_matrix
                 B_hatg[na_elements] = MLE_gls.x[:np.sum(na_elements)]
+                lambda_hatg = np.diag(MLE_gls.x[np.sum(na_elements):])  #np.sum(na_elements):len(MLE_gls.x) 
             else:
                 B_hatg = MLE_gls.x[:k*k].reshape(k,-1)
-                lambda_hatg = np.diag(MLE_gls.x[k*k+1:k*k+k])
+                lambda_hatg = np.diag(MLE_gls.x[k*k:k*k+k])
             
             ll_g = MLE_gls.fun
 
@@ -247,12 +251,13 @@ class SVAR_CV(tsbase.TimeSeriesModel):
         GLSE = GLSE[cc]
         if GLSE is not None:
             GLSE.reshape((k,-1))
-        MLEgls = MLE_gls_loop[cc]
+        MLE_gls = MLE_gls_loop[cc]
 
         # obtaining standard errors from inverse fisher information matrix
-        HESS = solve(MLE_gls.hes, np.eye(MLE_gls.hes.shape[0]))
+        # HESS = solve(MLE_gls.hes, np.eye(MLE_gls.hes.shape[0]))
+        HESS = MLE_gls.hess_inv
 
-        for i in range(HESS.shape()[0]):
+        for i in range(HESS.shape[0]):
             if(HESS[i,i] < 0):
                 HESS[:,i] = -HESS[:,i]
         
@@ -284,7 +289,7 @@ class SVAR_CV(tsbase.TimeSeriesModel):
                   'method': 'Changes in Volatility',
                   'SB': SB,
                   'A_hat': GLSE,
-                  'type': type,
+                  'type': type_,
                   'SB_character': SB_character,
                   'restrictions': restrictions,
                   'restriction_matrix': restriction_matrix,
@@ -341,21 +346,24 @@ class SVAR_CV(tsbase.TimeSeriesModel):
     
     def _wald_test(self, lambda_, sigma, restrictions):
         k = len(np.diag(lambda_))
-        k_list = list(itertools.combinations(range(1,(k+1)), 2))
-        betas = list(itertools.combinations(np.diag(lambda_), 2))
-        mask = [k * k + x[0] - restrictions, k * k + x[1] - restrictions] 
-        sigmas = np.apply_along_axis(lambda x: np.diag(sigma[mask,mask]), axis=1, arr=k_list)
-        cov_s = np.apply_along_axis(lambda x: sigma[mask,mask][0,1], axis=1, arr=k_list)
+        k_list = np.asarray(list(itertools.combinations(range(0,k), 2))).T
+        betas = np.asarray(list(itertools.combinations(np.diag(lambda_), 2))).T
+        # k, k_list and betas are correct
+        #def cal_sigma(x, restrictions, sigma):
+        #    print(x[0],x[1])
+        #    return np.diag(sigma[np.ix_([k * k + x[0] - restrictions, k * k + 1 + x[1] - restrictions],[k * k + x[0] - restrictions, k * k + 1 + x[1] - restrictions])) 
+        sigmas = np.apply_along_axis(lambda x: np.diag(sigma[np.ix_([k * k + x[0] - restrictions, k * k + x[1] - restrictions],[k * k + x[0] - restrictions, k * k + x[1] - restrictions])]), axis=0, arr=k_list)
+        cov_s = np.apply_along_axis(lambda x: sigma[np.ix_([k * k + x[0] - restrictions, k * k + x[1] - restrictions], [k * k + x[0] - restrictions, k * k + x[1] - restrictions])][0,1], axis=0, arr=k_list)
+        
+        p_value = np.asarray(list(map(lambda x: np.round(1 - chi2.cdf((betas[:,x] @ np.array([1,-1])) ** 2 / (np.sum(sigmas[:,x]) - 2 * cov_s[x]), 1), 2), range(0,(k*(k-1)+1)//2))))
+        test_stat = np.asarray(list(map(lambda x: np.round((betas[:,x]@np.array([1,-1]))**2 / (np.sum(sigmas[:,x]) - 2 * cov_s[x]),2), range(0, (k*(k-1)+1)//2))))
 
-        p_value = list(map(lambda x: np.round(1 - chi2.cdf((betas[:,x] @ [1,-1]) ** 2 / (np.sum(sigmas[:,x]) - 2 * cov_s[x]), 1), 2), range(1,k*(k-1)/2 +1)))
-
-        test_stat = list(map(lambda x: np.round((betas[:,x]@np.array([1,-1]))**2 / (np.sum(sigmas[:,x]) - 2 * cov_s[x]),2), range(1, k*(k-1)/2)))
-
-        combinations = itertools.combinations(range(1,k+1), 2)
+        combinations = np.asarray(list(itertools.combinations(range(1,k+1), 2)))
 
         template = "lambda_{}=lambda_{}"
         H_null = np.apply_along_axis(lambda x: template.format(x[0],x[1]), axis=1, arr=combinations)
 
-        wald_test = pd.DataFrame(data=[test_stat,p_value], index=H_null, columns=["Test statistic","p-Value"])
+        data = {"Test statistic": test_stat, "p-value": p_value}
+        wald_test = pd.DataFrame(data=data, index=H_null)
         return wald_test
 